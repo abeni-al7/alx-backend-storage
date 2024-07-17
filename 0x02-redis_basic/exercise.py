fe1865c2d0ff3fcac35b3d7928e7267b8635633e
abeni-al7 <abenezeralebachew3@gmail.com>
2024-07-17 23:39:55 +0300

    Implement Cache using redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''A module for a Cache class'''
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    '''A cache class'''
+    def __init__(self):
+        '''Initializing method'''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+    
+    def store(self, data: Union[int, bytes, float, str]) -> str:
+        '''Stores an input data into redis and returns the random key'''
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
